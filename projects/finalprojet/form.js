0a48c81620cbca7e50feddac1d8465052f9f73c2
truman <truetman34@gmail.com>
2026-04-07 19:42:55 -1000

    created final project

---
+++ projects/finalprojet/form.js
@@ -0,0 +1,48 @@
+document.getElementById('myForm').addEventListener('submit',function(event) {
+event.preventDefault();
+
+const fname = document.getElementById('fullname').value;
+const email = document.getElementById('email').value;
+const review = document.getElementById('review').value;
+const comment = document.getElementById('comments').value;
+
+if (!fname || !email) {
+    alert("You need a name and email.")
+    return;
+}
+
+if (!review) {
+    alert("You need check yes or no")
+    return;
+}
+
+if (!comment) {
+    alert("You must submit a comment")
+    return;
+}
+
+const formData = {
+    name: fname,
+    email:  email,
+    review: review,
+    comments: comment
+};
+
+console.log(formData);
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "submit.json", true);
+xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        alert("Form submitted successfully!");
+        const response = JSON.parse(xhr.responseText);
+        console.log(response);
+        //document.getElementById('myForm').reset();
+        document.getElementById('myForm').innerHTML = '';
+        document.getElementById('message').innerText = response.message;
+    } else if (xhr.readyState === 4) {
+        alert("Error submitting form.");
+    }
+};
+xhr.send(JSON.stringify(formData));
+});
